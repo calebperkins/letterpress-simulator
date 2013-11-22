75df7fb4903155055b95470d4ef12faf710d1005
Caleb Perkins <calebtperkins@gmail.com>
2013-11-22 00:46:42 -0800

    Updated project file.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject letterpress-bot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simulator for the game Letterpress."
+  :url "https://github.com/rofflebuster/letterpress-simulator"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
